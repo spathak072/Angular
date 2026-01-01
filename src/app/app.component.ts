cc5cae06ae887348c3d3082b6233fc1bbfe79405
spathak072 <saurabhpathak072@gmail.com>
2026-01-01 23:14:39 +0530

    Created the first custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./Header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+
 })
 export class AppComponent {}
