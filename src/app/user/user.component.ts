2837db3823066c6921954f9c90750ee1cfe2305e
spathak072 <saurabhpathak072@gmail.com>
2026-01-02 00:13:29 +0530

    bind the dynamic properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DUMMY_USERS, randomUser} from "../DUMMY_USER";
 
 @Component({
   selector: 'app-user',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+selectedUser = DUMMY_USERS[randomUser];
 }
